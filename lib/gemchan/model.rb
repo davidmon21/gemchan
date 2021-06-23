23be1ecdcdc5e8220fea71f36fe1809dbc0e1049
David Monfort <dmonfort89@icloud.com>
2021-06-23 10:47:51 -0500

    updated at dates, for op ordering, op id into op post

--- lib/gemchan/model.rb
+++ lib/gemchan/model.rb
@@ -9,6 +9,7 @@ CREATE TABLE boards (
 );
 CREATE TABLE ops (
     post_id INTEGER,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     board_id      INTEGER NOT NULL,
     FOREIGN KEY (board_id)
        REFERENCES boards (id)
