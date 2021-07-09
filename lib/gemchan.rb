e0e4903fa374bd8b213c98158a821d604f2a6f2e
quietok(david) <quietok@protonmail.com>
2021-07-09 09:25:07 -0500

    install files, executable, etc

--- lib/gemchan.rb
+++ lib/gemchan.rb
@@ -2,14 +2,12 @@ require_relative 'gemchan/version.rb'
 
 module Gemchan
   require 'fileutils'
-  require 'yaml'
+  
   require 'warden'
-  require 'bcrypt'
   require 'sinatra'
   require 'rmagick'
   require 'mimemagic'
   require 'digest/md5'
-  require 'securerandom'
   require_relative 'gemchan/controller.rb'
   require_relative 'gemchan/model.rb'
   require_relative 'gemchan/server.rb'
